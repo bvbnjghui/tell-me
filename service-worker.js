92b5af5d9466527f664025cab6fa5a5a1a1f8d9f
Linsly <39582509+bvbnjghui@users.noreply.github.com>
2025-08-05 16:34:45 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'mindreader-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  console.log('Service Worker: 安裝中...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Service Worker: 正在快取所有內容');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(error => {
+        console.error('Service Worker: 快取失敗', error);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Service Worker: 從快取中回傳', event.request.url);
+          return response;
+        }
+        console.log('Service Worker: 從網路中抓取', event.request.url);
+        return fetch(event.request);
+      })
+  );
+});
